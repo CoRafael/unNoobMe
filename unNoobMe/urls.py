1268c43b2065f07b6f4d8850186a2bb73eae92de
Rafael <CoRafael@hotmail.com>
2015-02-25 18:02:22 +0000

    dummy

--- unNoobMe/urls.py
+++ unNoobMe/urls.py
@@ -6,5 +6,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'unNoobMe.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+
+################################
+
     url(r'^admin/', include(admin.site.urls)),
 )
