68373bf46634cf12f14ba6df9237304a9d95f6be
Rafael <CoRafael@hotmail.com>
2015-02-26 00:10:15 +0000

    User Model Created. Also the admin page has the UserProfile Table listing all necessary fields requested by the database schema

--- noob/admin.py
+++ noob/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from noob.models import UserProfile
+
+
+class UserProfilePageAdmin(admin.ModelAdmin):
+    list_display = (
+        'username', 'firstname', 'last_name', 'email', 'city', 'points', 'last_login', 'date_joined', 'avatar',)
+
+
+admin.site.register(UserProfile, UserProfilePageAdmin)
+
+
 # Register your models here.
--- noob/models.py
+++ noob/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    # We are going to use the User predefined table
+    # info : https://docs.djangoproject.com/en/1.7/ref/contrib/auth/#django.contrib.auth.models.User.username
+    # basic info: {username,firstname,last_name.email,password, last_login??, date_joined?? -> we will discuss this!}
+    user = models.OneToOneField(User)
+
+    # The additional attributes we wish to include.
+    city = models.CharField(max_length=100)
+    points = models.IntegerField(default=0)
+    avatar = models.ImageField(upload_to='avatar_images', blank=True)
+
+    # Override the __unicode__() method to return out something meaningful!
+    def __unicode__(self):
+        return self.user.username
+
+    def username(self):
+        return self.user.username
+
+    def firstname(self):
+        return self.user.first_name
+
+    def last_name(self):
+        return self.user.last_name
+
+    def email(self):
+        return self.user.email
+
+    def last_login(self):
+        return self.user.last_login
+
+    def date_joined(self):
+        return self.user.date_joined
