11da01f0b9b3e97fae869773cc3aaf9941ef16f5
Rafael <CoRafael@hotmail.com>
2015-02-27 04:15:02 +0000

    Models are all ready up and running. Feel free using admin page and see all the tables with data

--- noob/admin.py
+++ noob/admin.py
@@ -1,14 +1,38 @@
 from django.contrib import admin
 
-from noob.models import UserProfile
+from noob.models import UserProfile, Advertisement, Interest, JobOffer, Rating
 
 
 class UserProfilePageAdmin(admin.ModelAdmin):
     list_display = (
-        'username', 'firstname', 'last_name', 'email', 'city', 'points', 'last_login', 'date_joined', 'picture',)
+        'username', 'firstname', 'last_name', 'email', 'city', 'points', 'last_login', 'date_joined', 'picture',
+        'get_interests',)
 
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = (
+        'advID', 'lesson', 'date', 'type_of_meeting', 'duration', 'active', 'get_interest')
+
+
+class InterestAdmin(admin.ModelAdmin):
+    list_display = (
+        'interestID', 'category', )
+
+
+class JobOfferAdmin(admin.ModelAdmin):
+    list_display = (
+        'get_user', 'get_add_id', 'offerID', 'accepted',)
+
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = (
+        'rateID', 'get_offer_id', 'get_author', 'rate', 'comment',)
 
 admin.site.register(UserProfile, UserProfilePageAdmin)
+admin.site.register(Advertisement, AdvertisementAdmin)
+admin.site.register(Interest, InterestAdmin)
+admin.site.register(JobOffer, JobOfferAdmin)
+admin.site.register(Rating, RatingAdmin)
 
 
 # Register your models here.
--- noob/models.py
+++ noob/models.py
@@ -2,16 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class UserProfile(models.Model):
-    # We are going to use the User predefined table
-    # info : https://docs.djangoproject.com/en/1.7/ref/contrib/auth/#django.contrib.auth.models.User.username
-    # basic info: {username,firstname,last_name.email,password, last_login??, date_joined?? -> we will discuss this!}
-    user = models.OneToOneField(User)
+class Interest(models.Model):
+    # do we really need an extra field to be a primary key?
+    # we know from theory that each model has a unique
+    category = models.CharField(max_length=100, default='Computer Science')
+    interestID = models.IntegerField(unique=True, primary_key=True)
 
+
+class UserProfile(models.Model):
     # The additional attributes we wish to include.
     city = models.CharField(max_length=100, default='Glasgow')
     points = models.IntegerField(default=0)
     picture = models.ImageField(upload_to='avatar_pictures', blank=True)
+    # other relationships
+    # We are going to use the User predefined table
+    # info : https://docs.djangoproject.com/en/1.7/ref/contrib/auth/#django.contrib.auth.models.User.username
+    # basic info: {username,firstname,last_name.email,password, last_login??, date_joined?? -> we will discuss this!}
+    user = models.OneToOneField(User)
+    userInterest = models.ManyToManyField(Interest)
 
     # Override the __unicode__() method to return out something meaningful!
     def __unicode__(self):
@@ -34,3 +42,54 @@ class UserProfile(models.Model):
 
     def date_joined(self):
         return self.user.date_joined
+
+    def get_interests(self):
+        return "\n".join([i.category for i in self.userInterest.all()])
+
+
+class Advertisement(models.Model):
+    advID = models.IntegerField(unique=True, primary_key=True)
+    active = models.BooleanField(default=True)
+    # sec? hr ? minite? Discuss!!!
+    duration = models.IntegerField()
+    type_of_meeting = models.CharField(max_length=150)
+    date = models.DateField()
+    lesson = models.CharField(max_length=150)
+    # other relationships
+    advInterest = models.OneToOneField(Interest)
+    user = models.OneToOneField(UserProfile)
+
+    def get_interest(self):
+        return self.advInterest.category
+
+    def get_user(self):
+        return self.user.username()
+
+
+class JobOffer(models.Model):
+    accepted = models.BooleanField(default=True)
+    offerID = models.IntegerField(unique=True, primary_key=True)
+    # other relationship
+    user = models.OneToOneField(UserProfile)
+    advertisement = models.OneToOneField(Advertisement)
+
+    def get_user(self):
+        return self.user.username()
+
+    def get_add_id(self):
+        return self.advertisement.advID
+
+
+class Rating(models.Model):
+    rateID = models.IntegerField(unique=True, primary_key=True)
+    rate = models.IntegerField(default=0)
+    comment = models.CharField(max_length=250, default='')
+    # other relationship
+    author = models.OneToOneField(UserProfile)
+    offer = models.OneToOneField(JobOffer)
+
+    def get_author(self):
+        return self.author.username()
+
+    def get_offer_id(self):
+        return self.offer.offerID
