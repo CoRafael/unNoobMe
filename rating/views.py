60a0848a5da89c620341b6109ea07e64416628a6
Finlay <finlaymaciver@Finlays-MBP.home>
2015-03-07 18:26:08 +0000

    Updated ratings
    Added more information to the ratings page

--- rating/views.py
+++ rating/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from base.models import User
 from base.models import *
 
 @login_required
-def latest_rating(request):
-    latest_rating_list = Rating.objects.order_by('id')[:10]
-    context = {'latest_rating': latest_rating_list}
+def latest_rating(request, username):
+    try:
+        user = UserProfile.objects.filter(user=User.objects.filter(username=username)[0])[0]
+
+        offers = JobOffer.objects.all()
+        advertisement = Advertisement.objects.all()
+        ratings = Rating.objects.filter(offer__in=offers)
+
+        context = {'userprofile': user, 'offers': offers, 'ratings': ratings, 'advertisement': advertisement}
+
+    except IndexError:
+        context = {}
+
     return render(request, 'ratings.html', context)
