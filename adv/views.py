4e48feeddc6a41916382fdfdec22a3d7c73a56b9
2156744b <2156744b@student.gla.ac.uk>
2015-03-22 21:23:02 +0000

    Added offers in advertisement modal

--- adv/views.py
+++ adv/views.py
@@ -4,10 +4,9 @@ from django.contrib.auth.decorators import login_required
 from base.models import *
 
 
-
-# return the latest 10 ACTIVE advertisments of the database
 @login_required
 def advertisement(request, id):
     adv = Advertisement.objects.filter(id=id)[0]
-    context = {'user': request.user, 'advertisement': adv}
+    offers = JobOffer.objects.filter(advertisement=id)
+    context = {'user': request.user, 'advertisement': adv, 'offers': offers}
     return render(request, 'advertisement/advertisement.html', context)
