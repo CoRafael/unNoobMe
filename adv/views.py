22dbbfd2f43c5cd3d671edf34a2bd7cb65cc6146
2156744b <2156744b@student.gla.ac.uk>
2015-03-22 23:04:37 +0000

    Updated advertisement response

--- adv/views.py
+++ adv/views.py
@@ -8,9 +8,5 @@ from base.models import *
 def advertisement(request, id):
     adv = Advertisement.objects.filter(id=id)[0]
     offers = JobOffer.objects.filter(advertisement=id)
-
-    print request.user.userprofile
-    print adv.user
-    print adv.user == request.user.userprofile
     context = {'user': request.user.userprofile, 'advertisement': adv, 'offers': offers}
     return render(request, 'advertisement/advertisement.html', context)
