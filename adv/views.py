68336a84f41b3fa380af73e35cd542d2fde6f8ed
2156744b <2156744b@student.gla.ac.uk>
2015-03-22 20:17:09 +0000

    Initial offer modal

--- adv/views.py
+++ adv/views.py
@@ -4,8 +4,10 @@ from django.contrib.auth.decorators import login_required
 from base.models import *
 
 
+
 # return the latest 10 ACTIVE advertisments of the database
 @login_required
 def advertisement(request, id):
-    context = {'id': id}
+    adv = Advertisement.objects.filter(id=id)[0]
+    context = {'user': request.user, 'advertisement': adv}
     return render(request, 'advertisement/advertisement.html', context)
