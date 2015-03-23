1f7a2dd3aa6741045ba90012fc42576905edb729
Finlay <finlaymaciver@glaroam2-158-115.wireless.gla.ac.uk>
2015-03-23 19:07:38 +0000

    Created a few tests
    Created a couple of tests following textbook examples, which are
    located in the base folder, as the models.py of other components are
    not implemented in their respective models.py file.
    Updated the models.py Advertisement section for a save method.

--- base/tests.py
+++ base/tests.py
@@ -1,1 +1,23 @@
-# Create your tests here.
+from django.test import TestCase
+from base.models import Advertisement
+from django.core.urlresolvers import reverse
+
+
+class AdvertMethodTests(TestCase):
+
+    def test_ensure_duration_is_positive(self):
+        ad = Advertisement(active=True, duration=-1, type_of_meeting='Skype', date='2012-09-04 06:00', advInterest_id=2, user_id=4)
+        ad.save()  # Save method converts the ad.duration to the absolute value i.e. from negative to positive
+        self.assertEqual((ad.duration >= 0), True)
+
+    def test_ensure_active_is_boolean(self):
+        ad = Advertisement(active=True, duration=-1, type_of_meeting='Skype', date='2012-09-04 06:00', advInterest_id=2, user_id=4)
+        ad.save()
+        self.assertIsInstance(ad.active, bool)
+
+
+class IndexViewTests(TestCase):
+
+    def test_index_view_with_no_interests(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 302)
