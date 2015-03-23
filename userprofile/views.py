04f52d03c019d5506e575f459c7a201b6bb176f0
2156744b <2156744b@student.gla.ac.uk>
2015-03-23 21:51:45 +0000

    Added interests selection in profile

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-from base.models import User, UserProfile, Rating, JobOffer
+from base.models import User, UserProfile, Rating, JobOffer, Interest
 
 
 @login_required
 def userprofile(request, username):
+    if request.method == 'POST' and request.POST.get('type') == 'updateinterests':
+        interests = request.POST.getlist('interestsselect')
+
+        user = request.user.userprofile
+
+        print user.get_interests()
+
+        user.userInterest.clear()
+
+        for i in interests:
+            user.userInterest.add(Interest.objects.get(id=i))
+
+        print user.get_interests()
+
     try:
         user = UserProfile.objects.filter(user=User.objects.filter(username=username)[0])[0]
         interests = user.userInterest.all()
@@ -14,9 +28,10 @@ def userprofile(request, username):
         # have to filter selected = true too with real data
 
         ratings = Rating.objects.filter(offer__in=offers)
+        interests = Interest.objects.all()
 
         context = {'username': username, 'userrequest': request.user.userprofile.username(), 'userprofile': user,
-                   'interests': interests,
+                   'interests': interests, 'interests': interests,
                    'ratings': ratings}
 
     except IndexError:
