928d44cc07618fd5f37aec3e457dc41073f9a2d4
Rafael <CoRafael@hotmail.com>
2015-03-24 03:24:43 +0000

    Added Contact Us page

--- base/urls.py
+++ base/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
                        url(r'^register/$', views.user_register, name='register'),
                        url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'base/register.html'}),
-                       url(r'^logout/$', views.user_logout, name='logout'), )
+                       url(r'^logout/$', views.user_logout, name='logout'),
+                       url(r'^contact_us/$', views.contact_us, name='contact_us'),)
 
--- base/views.py
+++ base/views.py
@@ -74,3 +74,7 @@ def user_logout(request):
 
     # Take the user back to the homepage.
     return HttpResponseRedirect('/')
+
+
+def contact_us(request):
+    return render(request, 'base/contact_us.html', {})
